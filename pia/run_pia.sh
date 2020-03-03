3394052da3a544b4397acbe2017cd1a64af9b077
Matin Gühmann <guehmann@t-online.de>
2020-03-03 13:50:22 +0000

    Make run_pia.sh determine its path automatically

--- pia/run_pia.sh
+++ pia/run_pia.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 aalength="30"			#minimum aminoacid sequence length
 search_type="single"	#single or set
 gene="r_opsin"			#gene(set) name
@@ -12,15 +23,15 @@ for file in *.fasta ; do
 	cd results_${file}
 	cp ../../${file} ./	
 
-	python ~/apps/pia/get_orfs_or_cdss.py $file fasta 1 ORF open top $aalength both ORF_nuc.fasta ORF_prot.fasta > stdout 2>&1
+	python "$DIR"/get_orfs_or_cdss.py $file fasta 1 ORF open top $aalength both ORF_nuc.fasta ORF_prot.fasta #> stdout 2>&1
 
-	perl ~/apps/pia/pia.pl ORF_prot.fasta $search_type $gene mafft $evalue $blasthits
+	perl "$DIR"/pia.pl ORF_prot.fasta $search_type $gene mafft $evalue $blasthits
 
-	perl ~/apps/pia/phylographics/makeRtrees.pl treeout.tab trees.pdf phylogram no None Rfile yes no >tree.R
+	perl "$DIR"/phylographics/makeRtrees.pl treeout.tab trees.pdf phylogram no None Rfile yes no >tree.R
 
 	R --vanilla < tree.R 2>log.txt
 
-	~/apps/pia/post_pia.sh
+	"$DIR"/post_pia.sh
 	cd ../../
 
 done
