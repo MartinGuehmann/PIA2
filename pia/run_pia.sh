1484cae19508997c72b1cb769fd44b9ba1057534
Matin Gühmann <guehmann@t-online.de>
2020-03-09 20:22:19 +0000

    Shelf attempt to use NCBI ORFfinder instead get_orfs_or_cdss.py
    Despite ORFfinder being a compiled binary, it does not seem to be faster than get_orfs_or_cdss.py. Additionally, it also has issues with sequence IDs and it does not save the descriptions of the entries. It limits the length of the ID to 50 characters, so that integrating the description into the ID by character replacement does not work either.

--- pia/run_pia.sh
+++ pia/run_pia.sh
@@ -11,6 +11,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 CURDIR="$(pwd)"
 
 aalength="30"			#minimum aminoacid sequence length
+ntlength="90"
 search_type="single"	#single or set
 gene="r_opsin"			#gene(set) name
 evalue="0.00000000000000000001"	#E-value threshold for BLAST search
@@ -25,7 +26,21 @@ for file in *.fasta ; do
 	cd results_${file}
 
 	# The file get_orfs_or_cdss.py is from the pico_galaxy repository. Install that next to your PIA2/pia/ directory. So this is two levels up of this script.
-	python "$DIR"/../../pico_galaxy/tools/get_orfs_or_cdss/get_orfs_or_cdss.py -i <(tr -d '\000' < $CURDIR/$file) -e open -m all --min_len $aalength --op ORF_prot.fasta
+#	python "$DIR"/../../pico_galaxy/tools/get_orfs_or_cdss/get_orfs_or_cdss.py -i <(tr -d '\000' < $CURDIR/$file) -e open -m all --min_len $aalength --op ORF_prot.fasta
+
+	# Find all the open reading frames
+	# ORFfinder does not like to have '|' in the sequence ID,
+	# so replace them with something we won't hopefully encounter
+	"$DIR"/../../Tools/ORFfinder \
+		-in <((tr -d '\000' < $CURDIR/$file) | tr '|' '@') \
+		-s 2 \
+		-ml $ntlength \
+		-n true \
+		-out ORF_prot1.fasta \
+			2> ORF_log.txt
+
+	sed -i 's/lcl\|//g' ORF_prot1.fasta
+	sed -i 's/\@/\|/g' ORF_prot1.fasta
 
 	perl "$DIR"/pia.pl ORF_prot.fasta $search_type $gene mafft $evalue $blasthits $numThreads
 
