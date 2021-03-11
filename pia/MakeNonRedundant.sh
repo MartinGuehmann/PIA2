12452b900d444189441e62a331a0d7b1a437621d
Matin Gühmann <guehmann@t-online.de>
2021-03-11 07:04:58 +0000

    Sort the sequences by ID before making redundant

--- pia/MakeNonRedundant.sh
+++ pia/MakeNonRedundant.sh
@@ -11,7 +11,15 @@ fileBase="${fileBase%.*}"
 outputFile="$filePath/$fileBase.nr90.$extension"
 numTreads=$(nproc)
 
-cd-hit -i $inputFile -o $outputFile -c 0.9 -M 0 -d 0 -T $numTreads >&2
+# This should work but it does not
+#cd-hit -i <(seqkit sort -j $numTreads $inputFile) -o $outputFile -c 0.9 -M 0 -d 0 -T $numTreads >&2
+
+tmpFile="$filePath/$fileBase.tmp.$extension"
+seqkit sort -j $numTreads -i $inputFile > $tmpFile
+
+cd-hit -i $tmpFile -o $outputFile -c 0.9 -M 0 -d 0 -T $numTreads >&2
+
+rm -f $tmpFile
 
 # Return the outfile
 echo $outputFile
