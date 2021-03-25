e07f9cd6ed1228e8a01d25df95c415d232e39ca9
Matin Gühmann <guehmann@t-online.de>
2021-03-25 00:00:23 +0000

    Make also DNA output files if the original ID contains "|"

--- pia/MakeDNAOutputFile.sh
+++ pia/MakeDNAOutputFile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Converts an amino acid sequence file to a DNA sequence file
+# Argument 1 (string): Amino acid sequence file to convert.
+# Argument 2 (string): DNA sequence file from that the amino acid sequences where translated
+# Reduces the number of sequences if the amino acid sequences are derived from the same DNA sequence
+#
+
 inputFile=$1
 dataBaseFile=$2
 
@@ -12,7 +19,7 @@ fileBase="${fileBase%.*}"
 outputFile="$filePath/$fileBase.nt.$extension"
 numTreads=$(nproc)
 
-seqIDs=$(seqkit seq -j $numTreads -n -i $inputFile | sed 's/|.*$//g' | sort | uniq)
+seqIDs=$(seqkit seq -j $numTreads -n -i $inputFile | sed 's/|ORF.*$//g' | sort | uniq)
 
 genePrefix="${fileBase%%.*}"
 
